54effe0a4f5cdb4544d87ce5b200a49e49f57705
Micah <micalobia2@gmail.com>
2021-02-04 23:55:17 -0700

    missed a file

--- src/main/java/micalobia/soul_magic/enchantment/Soulbound.java
+++ src/main/java/micalobia/soul_magic/enchantment/Soulbound.java
@@ -1,4 +1,4 @@
-package micalobia.soul_magic.enchants;
+package micalobia.soul_magic.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
